4a45affa0c8b8021d44496067026d33859962e8f
higa870 <htarou870@gmail.com>
2024-06-15 19:18:25 +0900

    second commit

--- sampleApp/sampleApp/QuizViewController.swift
+++ sampleApp/sampleApp/QuizViewController.swift
@@ -8,16 +8,50 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    @IBOutlet weak var quizNumberLabel: UILabel!
+    @IBOutlet weak var quizTextView: UITextView!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var answerButton4: UIButton!
+    
+    var csvArray: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
 
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
         // Do any additional setup after loading the view.
     }
     
     //ボタンを押したときに呼ばれる
     @IBAction func btnAction(sender: UIButton) {
         print(sender.tag)
+    }
+    
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
     }
     /*
     // MARK: - Navigation
